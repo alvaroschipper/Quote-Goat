69bd9cf7b9188aa8a25eec4f0ac24744b305a092
RemarkableGuy <alvaro.schipper@hotmail.com>
2016-12-05 23:30:12 +0100

    Added basic Spring Boot file structure

---
+++ quote-goat/src/main/java/com/alvaroschipper/QuoteGoatApplication.java
@@ -0,0 +1,12 @@
+package com.alvaroschipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteGoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteGoatApplication.class, args);
+	}
+}
